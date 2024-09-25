87604302e4791081a8ae4828631475bd44a253c7
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2024-09-25 17:16:52 -0300

    Refactor Plantas.js and PlantasCard.js
    - Refactor Plantas.js to include useEffect and useState hooks for fetching and filtering data from an API.
    - Add search functionality to filter data based on user input.
    - Create PlantasCard component to display individual plant data.
    
    Fixes #123

---
+++ src/components/PlantasCard.js
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+const PlantasCard = ({ data }) => {
+    return (
+        <View>
+            <Text>{data.name}</Text>
+        </View>
+    )
+}
--- src/pages/Plantas.js
+++ src/pages/Plantas.js
@@ -1,14 +1,77 @@
-import React from 'react';
-import { Text, SafeAreaView } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, TextInput, FlatList } from 'react-native';
 import { globalStyles } from '../styles/global';
+import { PlantasCard } from '../components/PlantasCard';
+import { axios } from 'axios';
 
 const Plantas  = () => {
+    // dados originais
+    const [ data, setData ] = useState([]);
 
-  return (
-    <SafeAreaView style={globalStyles.container}>
-      <Text>Tela do reino plantas</Text>
-    </SafeAreaView>
-  );
+    // dados filtrados
+    const [ filteredData, setFilteredData ] = useState([]);
+
+    // query de pesquisa
+    const [ searchQuery, setSearchQuery ] = useState('');
+
+    useEffect(() => {
+        try {
+            async function fetchData() {
+                const response = await axios.get('http://192.168.0.212/plantas ');
+                setData(response.plantas)
+
+                console.log(" response plantas" + response.plantas);
+            }
+
+            fetchData();
+
+        } catch (error) {
+            console.log(error);
+        }
+
+    }, []);
+
+    useEffect(() => {
+
+        function filterData() {
+            if (searchQuery !== '') {
+                const filteredDatas = data.filter((item) => {
+                    return item.name.includes(searchQuery);
+                });
+
+                setFilteredData(filteredDatas);
+
+            } else {
+
+                setFilteredData(data);
+
+            }
+
+            console.log("Filtrados :" + filteredData);
+        }
+
+        filterData();
+
+    },[searchQuery]);
+
+    return (
+        <SafeAreaView style={globalStyles.container}>
+            <TextInput
+
+            value={searchQuery}
+            onChangeText={setSearchQuery}
+            placeholder="Pesquisar..."
+            />
+
+            <FlatList 
+                data={filteredData}
+                keyExtractor={(item) => item.id}
+                renderItem={({ item }) => (
+                    <PlantasCard data={item}/>
+                )}
+            />
+        </SafeAreaView>
+    );
 };
 
 
