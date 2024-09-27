147518dbb4cf3a226320efcae26692c1cd2873ff
Alexandro Oliveira <113798156+alexandrooliveira87@users.noreply.github.com>
2024-09-26 22:01:57 -0300

    Consumo da API na tela Fungi e Craido a estilização

--- src/pages/Fungi.js
+++ src/pages/Fungi.js
@@ -1,15 +1,42 @@
-import React from 'react';
-import { Text, SafeAreaView } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Text, SafeAreaView, FlatList, View, Image, StyleSheet, Alert } from 'react-native';
+import axios from 'axios';
 import { globalStyles } from '../styles/global';
 
-const Fungos  = () => {
+const Fungos = () => {
+  
+  const [fungos, setFungos] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://192.168.0.15:3000/fungos")
+      .then((response) => {
+        setFungos(response.data);
+      })
+      .catch((err) => {
+        console.error(err);
+        Alert.alert("Erro ao buscar os dados");
+      });
+  }, []);
+
+  const renderItem = ({ item }) => (
+    <View style={globalStyles.cartao}>
+      <Image source={{ uri: item.image }} style={globalStyles.imagemCartao} />
+      <Text style={globalStyles.nomeCartao}>{item.name}</Text>
+      <Text style={globalStyles.descricaoCartao}>{item.description}</Text>
+      <Text style={globalStyles.detalheCartao}>Nutrição: {item.nutrition}</Text>
+      <Text style={globalStyles.detalheCartao}>Respiração: {item.respiration}</Text>
+    </View>
+  );
 
   return (
     <SafeAreaView style={globalStyles.container}>
-      <Text>Tela do reino fungi</Text>
+      <FlatList
+        data={fungos}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+      />
     </SafeAreaView>
   );
 };
-
 
 export default Fungos;
--- src/styles/global.js
+++ src/styles/global.js
@@ -5,5 +5,36 @@ export const globalStyles = StyleSheet.create({
       flex: 1,
       backgroundColor: '#f0f0f0',
       paddingVertical: 20,
+    },
+    cartao: {
+      backgroundColor: '#fff',
+      padding: 15,
+      marginVertical: 10,
+      marginHorizontal: 20,
+      borderRadius: 10,
+      shadowColor: '#000',
+      shadowOffset: { width: 0, height: 2 },
+      shadowOpacity: 0.3,
+      shadowRadius: 2,
+      elevation: 5,
+    },
+    imagemCartao: {
+      width: '100%',
+      height: 150,
+      borderRadius: 10,
+    },
+    nomeCartao: {
+      fontSize: 18,
+      fontWeight: 'bold',
+      marginVertical: 10,
+    },
+    descricaoCartao: {
+      fontSize: 14,
+      color: '#333',
+      marginBottom: 5,
+    },
+    detalheCartao: {
+      fontSize: 12,
+      color: '#666',
     }
   });
